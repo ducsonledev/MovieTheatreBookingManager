82f0e6129bb35957be360c7e8deefe685704900c
Son <72577766+ozon-1@users.noreply.github.com>
2023-06-28 20:56:59 +0200

    adding properties for screen/screening

--- src/main/java/com/movies/MovieTheatreBookingManager/entities/Screen.java
+++ src/main/java/com/movies/MovieTheatreBookingManager/entities/Screen.java
@@ -1,4 +1,16 @@
 package com.movies.MovieTheatreBookingManager.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
 public class Screen {
+    @Id
+    @GeneratedValue
+    private long screenId;
+    private long theatreId;
+    private int seatsNum;
 }
--- src/main/java/com/movies/MovieTheatreBookingManager/entities/Screening.java
+++ src/main/java/com/movies/MovieTheatreBookingManager/entities/Screening.java
@@ -1,4 +1,25 @@
 package com.movies.MovieTheatreBookingManager.entities;
 
-public class Screening {
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table
+@Data
+public class Screening implements Cloneable {
+
+    @Id
+    @GeneratedValue
+    private long screeningId;
+    private long theatreId;
+    private long screenId;
+    private String movieName;
+    private java.sql.Date screeningDate;
+    private java.sql.Time screeningTime;
+    private int bookedTickets;
+
+    public Object clone() throws CloneNotSupportedException{
+        return super.clone();
+    }
 }
