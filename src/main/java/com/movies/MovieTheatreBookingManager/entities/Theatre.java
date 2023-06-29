9cfba24fab6d032da4b282a576ded6f9b2d2e8da
Son <72577766+ozon-1@users.noreply.github.com>
2023-06-29 20:32:03 +0200

    added Theatre and Ticket properties

--- src/main/java/com/movies/MovieTheatreBookingManager/entities/Theatre.java
+++ src/main/java/com/movies/MovieTheatreBookingManager/entities/Theatre.java
@@ -1,4 +1,17 @@
 package com.movies.MovieTheatreBookingManager.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
 public class Theatre {
+
+    @Id
+    @GeneratedValue
+    private int theatreId;
+    private String theatreName;
+    private String theatreCity;
 }
--- src/main/java/com/movies/MovieTheatreBookingManager/entities/Ticket.java
+++ src/main/java/com/movies/MovieTheatreBookingManager/entities/Ticket.java
@@ -1,4 +1,15 @@
 package com.movies.MovieTheatreBookingManager.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
 public class Ticket {
+
+    @Id
+    @GeneratedValue
+    private int ticketId;
+    private long screeningId;
+    private int seatNum;
 }
