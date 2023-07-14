46132ad39577755797d4411b0ab90427a6a52b0e
Son <72577766+ozon-1@users.noreply.github.com>
2023-07-14 16:07:51 +0200

    add controller for seats

---
+++ src/main/java/com/movies/MovieTheatreBookingManager/controller/SeatsController.java
@@ -0,0 +1,22 @@
+package com.movies.MovieTheatreBookingManager.controller;
+
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.logging.Logger;
+
+@Controller
+@RequestMapping("/api/v1/seats")
+public class SeatsController {
+    private final Logger LOGGER = (Logger) LoggerFactory.getLogger(this.getClass());
+
+    @GetMapping
+    public String bookSeats(@RequestParam(value = "count") int seatCount, Model model) {
+        model.addAttribute("count", seatCount);
+        return "seats";
+    }
+}
--- src/main/java/com/movies/MovieTheatreBookingManager/controller/UserAuthController.java
+++ src/main/java/com/movies/MovieTheatreBookingManager/controller/UserAuthController.java
@@ -6,15 +6,13 @@ import com.movies.MovieTheatreBookingManager.dto.AuthenticationResponse;
 import com.movies.MovieTheatreBookingManager.dto.RegisterRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
-public class AuthenticationController {
+public class UserAuthController {
 
     private final AuthenticationService service;
 
@@ -30,5 +28,10 @@ public class AuthenticationController {
             @RequestBody AuthenticationRequest request
     ) {
         return ResponseEntity.ok(service.authenticate(request));
+    }
+
+    @GetMapping
+    public String welcome(Model model) {
+        return "welcome";
     }
 }
