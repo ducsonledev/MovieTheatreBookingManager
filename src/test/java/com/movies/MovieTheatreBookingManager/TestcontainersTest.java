1f680d46b651fd3205292a48f7a1e67527265e3e
Son <72577766+ozon-1@users.noreply.github.com>
2023-09-01 18:05:16 +0200

    testcontainers postgres db for unit testing

---
+++ src/test/java/com/movies/MovieTheatreBookingManager/TestcontainersTest.java
@@ -0,0 +1,26 @@
+package com.movies.MovieTheatreBookingManager;
+
+import org.junit.jupiter.api.Test;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Testcontainers
+public class TestcontainersTest {
+
+    @Container
+    private static final PostgreSQLContainer<?> postgreSQLContainer =
+            new PostgreSQLContainer<>("postgres:latest")
+                    .withDatabaseName("sonscode-dao-unit-test")
+                    .withUsername("sonscode")
+                    .withPassword("password");
+
+    @Test
+    void canStartPostgresDB() {
+       assertThat(postgreSQLContainer.isRunning()).isTrue();
+       assertThat(postgreSQLContainer.isCreated()).isTrue();
+       // assertThat(postgreSQLContainer.isHealthy()).isTrue();
+    }
+}
