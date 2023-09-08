a378cbe4c745cec981d133acdf06ffa7f326e542
Son <72577766+ozon-1@users.noreply.github.com>
2023-09-08 23:09:17 +0200

    Set Unit Test for `ScreeningRepositoryTest`

---
+++ src/test/java/com/movies/MovieTheatreBookingManager/repositories/ScreeningRepositoryTest.java
@@ -0,0 +1,37 @@
+package com.movies.MovieTheatreBookingManager.repositories;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+
+class ScreeningRepositoryTest {
+
+    @Autowired
+    private ScreeningRepository underTest;
+    @Test
+    void findByScreeningDate() {
+        // Given
+
+        // When
+
+        // Then
+    }
+
+    @Test
+    void findByMovieName() {
+        // Given
+
+        // When
+
+        // Then
+    }
+
+    @Test
+    void findByMovieNameAndTheatreIdAndScreeningDateAndScreeningTime() {
+        // Given
+
+        // When
+
+        // Then
+    }
+}
