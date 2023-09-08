9c62844b944a7ad6097a848cf2c2731ca1c495c7
Son <72577766+ozon-1@users.noreply.github.com>
2023-09-08 23:01:07 +0200

    Unit Test findByMovieName() from `MovieRepositoryTest`

--- src/test/java/com/movies/MovieTheatreBookingManager/repositories/MovieRepositoryTest.java
+++ src/test/java/com/movies/MovieTheatreBookingManager/repositories/MovieRepositoryTest.java
@@ -2,13 +2,19 @@ package com.movies.MovieTheatreBookingManager.repositories;
 
 import com.movies.MovieTheatreBookingManager.AbstractTestcontainers;
 import com.movies.MovieTheatreBookingManager.entities.Movie;
+import org.jsoup.HttpStatusException;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.io.IOException;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
@@ -23,11 +29,29 @@ class MovieRepositoryTest extends AbstractTestcontainers {
     @Test
     void findByMovieName() {
         // Given
+        // https://www.imdb.com/title/tt0114709/ // Toy Story // Animation
         Movie movie = new Movie();
+        String movieName = "Toy Story";
+        movie.setMovieName(movieName);
+        movie.setMovieTags("Animation");
+        Document movieLensPage = null;
+        try {
+            movieLensPage = Jsoup.connect("https://www.imdb.com/title/tt" + "0114709").get();
+        } catch (HttpStatusException e) {
+            return;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
+        if (movieLensPage != null) {
+            Element image = movieLensPage.getElementsByClass("ipc-poster").first().children().first().children().first();
+            movie.setMoviePosterUrl(image.attr("src"));
+        }
+        underTest.save(movie);
         // When
-
+        var actual = underTest.findByMovieName(movieName);
         // Then
+        assertThat(actual.getMovieName()).isEqualTo(movieName);
     }
 
     @Test
