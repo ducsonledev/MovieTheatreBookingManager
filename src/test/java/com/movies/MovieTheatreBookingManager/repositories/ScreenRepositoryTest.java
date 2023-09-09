3ec54c7404e4236ca8c23afc8ee4810873609222
Son <72577766+ozon-1@users.noreply.github.com>
2023-09-09 18:03:02 +0200

    set up unit test screen

---
+++ src/test/java/com/movies/MovieTheatreBookingManager/repositories/ScreenRepositoryTest.java
@@ -0,0 +1,26 @@
+package com.movies.MovieTheatreBookingManager.repositories;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ScreenRepositoryTest {
+
+    @Test
+    void findByScreenId() {
+        // Given
+
+        // When
+
+        // Then
+    }
+
+    @Test
+    void findByTheatreId() {
+        // Given
+
+        // When
+
+        // Then
+    }
+}
