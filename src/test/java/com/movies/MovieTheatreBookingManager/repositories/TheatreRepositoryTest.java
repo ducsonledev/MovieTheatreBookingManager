1a500e92b044707fb36c952eec8fe17ccb8f4b56
Son <72577766+ozon-1@users.noreply.github.com>
2023-09-10 21:04:49 +0200

    set up theatre unit testing

---
+++ src/test/java/com/movies/MovieTheatreBookingManager/repositories/TheatreRepositoryTest.java
@@ -0,0 +1,29 @@
+package com.movies.MovieTheatreBookingManager.repositories;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TheatreRepositoryTest {
+
+    @Autowired
+    private TheatreRepository underTest;
+    @Test
+    void findByTheatreId() {
+        // Given
+
+        // When
+
+        // Then
+    }
+
+    @Test
+    void findByTheatreNameAndTheatreCity() {
+        // Given
+
+        // When
+
+        // Then
+    }
+}
