33507a69e50b9bfbe233dccccdefe1a3fbac567d
Son <72577766+ozon-1@users.noreply.github.com>
2023-09-16 22:01:42 +0200

    integation test set up

---
+++ src/test/java/com/movies/MovieTheatreBookingManager/journey/UserIntegrationTest.java
@@ -0,0 +1,11 @@
+package com.movies.MovieTheatreBookingManager.journey;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+@SpringBootTest(webEnvironment = RANDOM_PORT) // will not start a server
+public class UserIntegrationTest {
+
+
+}
