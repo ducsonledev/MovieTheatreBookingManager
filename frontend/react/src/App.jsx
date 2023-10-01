e29909996d2f9c6eb03f67ba64322df8ccc9f299
Son <72577766+ducsonledev@users.noreply.github.com>
2023-10-01 16:24:35 +0200

    sidebar component

--- frontend/react/src/App.jsx
+++ frontend/react/src/App.jsx
@@ -1,12 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import SideBarWithHeader from './components/shared/SideBar.jsx'
+import { Button } from '@chakra-ui/react'
 
 function App() {
   return (
     <>
-        <p>Test</p>
+        <SideBarWithHeader/>
     </>
   )
 }
